7f35ec7c7c318df8bc4f8d3bd463d102bc2d794f
preethamcoder <preetham23415@gmail.com>
2024-07-25 19:07:14 -0400

    Display array elements

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -1,6 +1,10 @@
 public class Insertion_Sort{
     public static void main(String[] args){
-        arr = int[] {3, 5, 15, 6, 2, 4, 62, 3, 7};
-
+        int[] arr = new int[] {3, 5, 15, 6, 2, 4, 62, 3, 7};
+        // Dsplay elements in array
+        int length = arr.length;
+        for (int ind = 0; ind < length; ind++){
+            System.out.println(arr[ind]);
+        }
     }
 }
