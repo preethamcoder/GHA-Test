832bb815eb86d1684840675ad0215cf05ab2e0d3
preethamcoder <preetham23415@gmail.com>
2024-07-25 19:20:47 -0400

    thank you, semicolon

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -10,7 +10,7 @@ public class Insertion_Sort{
         int[] res = ins_sort(arr);
         System.out.println(Arrays.toString(res));
     }
-    public static void ins_sort(int[] array){
+    public static int[] ins_sort(int[] array){
             int len = array.length;
             for (int ind = 1; ind < len; ind++){
                 int curr = array[ind];
@@ -24,6 +24,6 @@ public class Insertion_Sort{
             for (int tmp = 0; tmp < array.length; tmp++){
                 System.out.println(array[tmp]);
             }
-            return arr;
+            return array;
         }
 }
