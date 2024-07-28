04b3bf74d382dc7148586230aa61273f771a4390
Preetham <sthelluri1@student.gsu.edu>
2024-07-28 16:09:07 -0400

    Add to begining of linked list

--- LinkedList.java
+++ LinkedList.java
@@ -9,7 +9,14 @@ public class LinkedList{
         }
     }
     Node head = null;
-    public void addFirst(){
-        
+    public void addFirst(int val){
+        Node tmp = new Node(val);
+        tmp.next = head;
+        head = tmp;
+    }
+    public static void main(String[] args) {
+        LinkedList lst = new LinkedList();
+        lst.addFirst(20);
+        System.out.println(lst.head.value);
     }
 }
