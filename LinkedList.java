a8e7da920943967c2d1f2159b65e71a72d6d2250
Preetham <sthelluri1@student.gsu.edu>
2024-07-28 17:08:44 -0400

    Added node at position

--- LinkedList.java
+++ LinkedList.java
@@ -9,15 +9,18 @@ public class LinkedList{
         }
     }
     Node head = null;
+    int elems = 0;
     public void addFirst(int val){
         Node tmp = new Node(val);
         tmp.next = head;
         head = tmp;
+        elems += 1;
     }
     public void addLast(int val){
         Node tmp = new Node(val);
         if(head == null){
             head = tmp;
+            elems += 1;
             return;
         }
         Node nd = head;
@@ -25,14 +28,39 @@ public class LinkedList{
             nd = nd.next;
         }
         nd.next = tmp;
+        elems += 1;
+    }
+    public void addAtN(int v, int pos){
+        if(pos > elems || pos < 0){
+            System.out.println("Invalid index");
+            return;
+        }else if(pos == 0){
+            addFirst(v);
+            return;
+        }else if(pos == elems){
+            addLast(v);
+            return;
+        }else{
+            Node nd = head;
+            int cter = 0;
+            Node adder = new Node(v);
+            while(cter < pos){
+                nd = nd.next;
+                cter += 1;
+            }
+            adder.next = nd.next;
+            nd.next = adder;
+        }
+
     }
     public static void main(String[] args) {
         LinkedList lst = new LinkedList();
         lst.addLast(-20);
         lst.addFirst(20);
-        lst.addLast(2);
-        lst.addLast(100);
-        lst.addFirst(80);
+        lst.addAtN(-100, 1);
+        // lst.addLast(2);
+        // lst.addLast(100);
+        // lst.addFirst(80);
 
         Node tmp = lst.head;
         while(tmp != null){
