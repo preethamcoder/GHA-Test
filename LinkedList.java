8d6acf4a1438064e8dbfc8d3edd3f3b602cb53b9
Preetham <sthelluri1@student.gsu.edu>
2024-07-28 15:51:29 -0400

    Implementing linkedlist

---
+++ LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList{
+    static class Node{
+        int value;
+        Node next;
+    
+        public Node(int val){
+            this.value = val;
+            next = null;
+        }
+    }
+    Node head = null;
+    public void addFirst(){
+        
+    }
+}
